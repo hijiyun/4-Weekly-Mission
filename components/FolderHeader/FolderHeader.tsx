9000ce77d9f7d0d18794c807d9454e12ffca439b
박지윤 <tkrhkfld123@naver.com>
2024-03-28 11:33:32 +0900

    move: 파일 next로 이동

---
+++ components/FolderHeader/FolderHeader.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import FolderProfile from "../FolderProfile";
+import '@/components/FolderHeader/FolderHeader'
+import Image from "next/image";
+import Link from "next/link";
+import logo from '@/images/logo.svg'
+
+
+function FolderHeader() {
+  return (
+    <header className="header">
+      <div className="header-logo">
+        <Link href="/">
+          <Image src={logo} alt="로고" />
+        </Link>
+      </div>
+      <div className="header-content">
+        <FolderProfile />
+      </div>
+    </header>
+  );
+}
+
+export default FolderHeader;
---
+++ components/Header/Header.tsx
@@ -0,0 +1,36 @@
+"use client";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import Profile from "@/components/profile";
+import "@/components/Header/Header";
+import Image from "next/image";
+import logo from '@/images/logo.svg'
+
+function Header() {
+  const router = useRouter();
+  const [isFolderPage, setIsFolderPage] = useState(false);
+
+  useEffect(() => {
+    if (router.isReady) {
+      setIsFolderPage(router.pathname === "/folder");
+    }
+  }, [router.isReady, router.pathname]);
+
+  const headerClass = isFolderPage ? "folder-page-header" : "default-header";
+
+  return (
+    <header className={headerClass}>
+      <div className="header-logo">
+        <Link href="/">
+          <Image src={logo} alt="로고" />
+        </Link>
+      </div>
+      <div className="header-content">
+        <Profile />
+      </div>
+    </header>
+  );
+}
+
+export default Header;
