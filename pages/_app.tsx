91ea215a4f144f0c9b583d55e1742f9d4e4effd3
박지윤 <tkrhkfld123@naver.com>
2024-03-28 16:24:58 +0900

    add: css 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,17 @@
 import type { AppProps } from 'next/app'
+import '@/components/FolderHeader/FolderHeader.css'
+import "@/components/LinkForm/LinkForm.css";
+import "@/components/FloatingActionButton/FloatingActionButton.css";
+import '@/components/ActionContainer/ActionContainer.css'
+import "@/components/SearchBar/SearchBar.css";
+import "@/components/FolderList/FolderList.css";
+import "@/components/FolderContent/FolderContent.css";
+import "@/components/LinkCard/LinkCard.css";
+import "@/components/Footer/Footer.css";
+import '@/components/Modal/Modal.css'
+import "@/components/Nav/Nav.css";
+import "@/components/Cardlist/Cardlist.css";
+import "@/components/Input/Input.css";
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
