1f0f3aa8124dbd5a5e83a81f87e7e93bb6c6469e
박지윤 <tkrhkfld123@naver.com>
2024-04-05 23:45:12 +0900

    chore: api baseUrl 환경 변수 추가

--- components/api/instance.ts
+++ components/api/instance.ts
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
+const baseUrl = process.env.NEXT_PUBLIC_API_KEY
+
 export const instance = axios.create({
-    baseURL: 'https://bootcamp-api.codeit.kr/api/',
+    baseURL: baseUrl,
     timeout: 10000, 
     headers: {
         'Content-Type': 'application/json'
